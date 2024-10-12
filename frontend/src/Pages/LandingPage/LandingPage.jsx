eae72d6721e46ec26f36234d4667358fa86515b7
Anjula-Himashi <anjulahimashi2@gmail.com>
2024-10-12 23:33:28 +0530

    landing page completed

--- frontend/src/Pages/LandingPage/LandingPage.jsx
+++ frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -4,29 +4,38 @@ import './LandingPage.scss';
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <div className="plans">
-        <div className="plan-item">
-          <p>UnitedHealthcare</p>
-          <p>77% Plan Match for BloomTech</p>
-        </div>
-        <div className="plan-item">
-          <p>Humana</p>
-          <p>72% Plan Match for BloomTech</p>
-        </div>
-        <div className="plan-item">
-          <p>Aetna</p>
-          <p>82% Plan Match for BloomTech</p>
-        </div>
-        <div className="plan-item">
-          <p>Anthem Blue Cross</p>
-          <p>91% Plan Match for BloomTech</p>
-        </div>
+
+      <div className="left">
+          <div className="plan-item">
+            <p>UnitedHealthcare</p>
+            <p>77% Plan Match for BloomTech</p>
+          </div>
+          <div className="plan-item">
+            <p>Humana</p>
+            <p>72% Plan Match for BloomTech</p>
+          </div> 
       </div>
-      <h1>Predict Your Health Risks with AI</h1>
-      <p>Find Possible Health Issues Based on Symptoms in Seconds</p>
-      <button className="cta-button">Predict Now</button>
-      <button className="google-signup">Sign Up with Google</button>
+        
+      <div className='middle'>
+          <h1>Predict Your Health Risks with AI</h1>
+          <p>Find Possible Health Issues Based on Symptoms in Seconds</p>
+          <button className="cta-button">Predict Now</button>
+          <button className="google-signup">Sign Up with Google</button>
+      </div>
+
+      <div className='right'>
+          <div className="plan-item">
+            <p>Aetna</p>
+            <p>82% Plan Match for BloomTech</p>
+          </div>
+          <div className="plan-item">
+            <p>Anthem Blue Cross</p>
+            <p>91% Plan Match for BloomTech</p>
+          </div>
+      </div>
+
     </div>
+    
   );
 }
 
