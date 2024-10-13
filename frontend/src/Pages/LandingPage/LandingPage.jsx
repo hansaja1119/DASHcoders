d59d145050caef659f2ac19dce6d1757327755d5
Anjula-Himashi <anjulahimashi2@gmail.com>
2024-10-13 18:13:56 +0530

    plan-items completed

--- frontend/src/Pages/LandingPage/LandingPage.jsx
+++ frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './LandingPage.scss';
 import { useNavigate } from 'react-router-dom';
+import company from '../../Icons/company.avif'
 
 const LandingPage = () => {
   const navigate = useNavigate();
@@ -13,17 +14,23 @@ const LandingPage = () => {
     <div className="landing-page">
       <div className="left">
         <div className="plan-item">
+        <img src={company} alt="image 01" className='image'/>
+        <div className='detail'>
           <p>UnitedHealthcare</p>
           <p>77% Plan Match for BloomTech</p>
         </div>
+        </div>
         <div className="plan-item">
+        <img src={company} alt="image 01" className='image'/>
+        <div className='detail'>
           <p>Humana</p>
           <p>72% Plan Match for BloomTech</p>
+          </div>
         </div>
       </div>
 
-      <div className="middle">
-        <h1>Predict Your Health Risks with AI</h1>
+      <div className="Topic">
+        <h1 className='gradient-text'>Predict Your Health Risks with AI</h1>
         <p>Find Possible Health Issues Based on Symptoms in Seconds</p>
         <div className='buttons'>
         <button className="cta-button" onClick={handlePredictNowClick}>
@@ -35,12 +42,18 @@ const LandingPage = () => {
 
       <div className="right">
         <div className="plan-item">
+        <img src={company} alt="image 01" className='image'/>
+        <div className='detail'>
           <p>Aetna</p>
           <p>82% Plan Match for BloomTech</p>
+          </div>
         </div>
         <div className="plan-item">
+          <img src={company} alt="image 01" className='image'/>
+          <div className='detail'>
           <p>Anthem Blue Cross</p>
           <p>91% Plan Match for BloomTech</p>
+          </div>
         </div>
       </div>
     </div>
