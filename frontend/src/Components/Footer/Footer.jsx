071ffaa2bdce793cde16c27f8770d9117854bf91
Anjula-Himashi <anjulahimashi2@gmail.com>
2024-10-14 19:25:48 +0530

    footer changed

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import './Footer.scss';
-import company from '../../Icons/company.avif';
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram } from 'react-icons/fa'; // Importing social media icons
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="partners">
-        <img src={company} alt="Humana" />
-        <img src={company} alt="Anthem" />
-        <img src={company} alt="UnitedHealthcare" />
-        <img src={company} alt="Aetna" />
-        <img src={company} alt="Cigna" />
+        <p>Connect with us on social media:</p>
+        <div className="social-links">
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebookF /></a>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><FaLinkedinIn /></a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+        </div>
       </div>
       <div className="footer-info">
-        <p>How we can help you</p>
+        <p>How can I help you?</p>
+        <p>Contact Us: <a href="mailto:support@remedpredict.com">support@remedpredict.com</a></p>
+        <p >&copy; {new Date().getFullYear()} Remed Predict. All rights reserved.</p>
       </div>
     </footer>
   );
 }
 
 export default Footer;
+
+
